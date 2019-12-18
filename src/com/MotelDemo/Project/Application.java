b589f5e5f96d87caa0b33be154a359bdb9cef318
hemachandaluri <40641222+hemachandaluri@users.noreply.github.com>
2019-12-18 15:30:49 -0500

    Changes

--- src/com/MotelDemo/Project/Application.java
+++ src/com/MotelDemo/Project/Application.java
@@ -1,56 +1,115 @@
 package com.MotelDemo.Project;
 
-import java.security.Provider.Service;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import MotelDemoProject.CatergoryRequirements;
+import MotelDemoProject.Service;
 
 public class Application {
-	
+
 	ArrayList<CatergoryRequirements> categories = new ArrayList<CatergoryRequirements>();
 	ArrayList<Service> services = new ArrayList<Service>();
-	Hashmap<String, List<?>> tempMap = new HashMap<>();
+	HashMap<String, List<?>> tempMap = new HashMap<>();
 
+	/*
+	 * Create method and pass list and return the even data list
+	 * 
+	 */
 
-	public static void main(String[] args) { 
-		Application application= new Application();
+	public static void main(String[] args) {
+		Application application = new Application();
 		application.AddData();
 		application.addServices();
-		
+		//String tempValue = application.returnGivenNumber(10);
+		//System.out.println("value from givenMethod :: " + tempValue);
 
 	}
+
+	
+	  public List<Service> evenDatalist(ArrayList<Service> temp) {
+		 /**For each ASerice in the list 
+		  * Wee return listand add required service to that list
+		  * 
+		  * 
+		  */
+		  
+		  //return result list
+		  ArrayList<Service> mServices = new ArrayList<Service>();	
+		  
+		  //iterate over each service in temp
+		  for(Service service: temp) {
+			  //logic for checking if service ID is even
+			  if(service.getService_id()%2==0) {
+				  mServices.add(service);
+			  }
+			  
+		  }
+		  
+		  //return result
+		  return mServices;
+	  }
+
+	  public List<String> getPrices(ArrayList<Service> services){
+		  ArrayList<String> prices = new ArrayList<>();
+		  for(Service service: services) {
+			  prices.add(Float.toString(service.getPrice()));
+		  }
+		  
+		  return prices;
+	  }
+	 
+
+/*	public String returnGivenNumber(int value) {
+		return value+1+"";
+	}*/
+
 //AddServiceData
 	public void addServices() {
-		Service service1 = new Service(1, "a", 9.99f);
-		Service service2 = new Service(1, "b", 8.99f);
-		Service service3 = new Service(1, "c", 7.99f);
-		Service service4 = new Service(1, "d", 6.99f);
-		Service service5 = new Service(1, "e", 5.99f);
+		Service service1 = new Service(1l, "a", 9.99f);
+		Service service2 = new Service(2l, "b", 8.99f);
+		Service service3 = new Service(3l, "c", 7.99f);
+		Service service4 = new Service(4l, "d", 6.99f);
+		Service service5 = new Service(5l, "e", 5.99f);
 		services.add(service1);
 		services.add(service2);
 		services.add(service3);
 		services.add(service4);
 		services.add(service5);
-		
+		for (Service s : services) {
+			if (s.getService_id() % 2 == 1)
+				System.out.println("The odd service Ids are" + s);
+		}
+
 	}
-	
+
 //AddData
 	public void AddData() {
-		CatergoryRequirements CatergoryRequirements = new CatergoryRequirements(1l, "bed",2, true, true, true, 2, 10.99f, 3.99f, true,true, true);
-		Categories CatergoryRequirements1 = new CatergoryRequirements1 (1l, "bed",2, true, true, true, 2, 10.99f, 3.99f, true,true, true);
-		Categories CatergoryRequirements2 = new CatergoryRequirements2
-		Categories CatergoryRequirements3 = new CatergoryRequirements3
-		Categories CatergoryRequirements4 = new CatergoryRequirements4
-		
-		categories.add(CatergoryRequirements)
-		categories.add(CatergoryRequirements1)
-		categories.add(CatergoryRequirements2)
-		categories.add(CatergoryRequirements3)
-		categories.add(CatergoryRequirements4)
+		CatergoryRequirements cat1 = new CatergoryRequirements(1l, "bed", 2, true, true, true, 2, 10.99f, 3.99f, true,
+				true, true);
+		CatergoryRequirements cat2 = new CatergoryRequirements(2l, "bed", 3, true, true, true, 2, 20.99f, 3.99f, true,
+				true, true);
+		CatergoryRequirements cat3 = new CatergoryRequirements(3l, "bed", 4, true, true, true, 2, 30.99f, 3.99f, true,
+				true, true);
+		CatergoryRequirements cat4 = new CatergoryRequirements(4l, "bed", 5, true, true, true, 2, 40.99f, 3.99f, true,
+				true, true);
+		CatergoryRequirements cat5 = new CatergoryRequirements(5l, "bed", 6, true, true, true, 2, 50.99f, 3.99f, true,
+				true, true);
+
+		categories.add(cat1);
+		categories.add(cat2);
+		categories.add(cat3);
+		categories.add(cat4);
+		categories.add(cat5);
+		for (CatergoryRequirements cr : categories) {
+			if (cr.getId() % 2 == 0)
+				System.out.println("The even Caterory IDs are " + cr);
+
+		}
 	}
-	
-//Update
-//Delete
-	
+
+//Edit and get
+//Delete and get
+
 }
